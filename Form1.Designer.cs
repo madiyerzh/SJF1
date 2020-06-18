57b46f752a438031e3323312d586251ff04551c3
madiyerzh <67119660+madiyerzh@users.noreply.github.com>
2020-06-19 00:35:37 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SJF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtrata = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.LV = new System.Windows.Forms.ListView();
+            this.txtJlhproses = new System.Windows.Forms.TextBox();
+            this.LJlhProses = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "SJF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtrata
+            // 
+            this.txtrata.Location = new System.Drawing.Point(393, 335);
+            this.txtrata.Name = "txtrata";
+            this.txtrata.Size = new System.Drawing.Size(77, 20);
+            this.txtrata.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Ratio TA";
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(81, 338);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(77, 20);
+            this.txttotal.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Total TA";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(187, 301);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(37, 57);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(60, 29);
+            this.btnGenerate.TabIndex = 13;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // LV
+            // 
+            this.LV.Location = new System.Drawing.Point(6, 92);
+            this.LV.Name = "LV";
+            this.LV.Size = new System.Drawing.Size(464, 203);
+            this.LV.TabIndex = 12;
+            this.LV.UseCompatibleStateImageBehavior = false;
+            // 
+            // txtJlhproses
+            // 
+            this.txtJlhproses.Location = new System.Drawing.Point(37, 19);
+            this.txtJlhproses.Multiline = true;
+            this.txtJlhproses.Name = "txtJlhproses";
+            this.txtJlhproses.Size = new System.Drawing.Size(60, 32);
+            this.txtJlhproses.TabIndex = 11;
+            this.txtJlhproses.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LJlhProses
+            // 
+            this.LJlhProses.AutoSize = true;
+            this.LJlhProses.BackColor = System.Drawing.SystemColors.Control;
+            this.LJlhProses.Location = new System.Drawing.Point(3, 3);
+            this.LJlhProses.Name = "LJlhProses";
+            this.LJlhProses.Size = new System.Drawing.Size(108, 13);
+            this.LJlhProses.TabIndex = 10;
+            this.LJlhProses.Text = "Number of Processes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 365);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtrata);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.LV);
+            this.Controls.Add(this.txtJlhproses);
+            this.Controls.Add(this.LJlhProses);
+            this.Name = "Form1";
+            this.Text = "SJF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtrata;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ListView LV;
+        private System.Windows.Forms.TextBox txtJlhproses;
+        private System.Windows.Forms.Label LJlhProses;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SJF
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            data = new Data[0];
+            listview();
+        }
+        private Data[] data;
+        struct Data
+        {
+            public string nama;
+            public int arrived, longt, waitt, restt, donet, startt, TA;
+        }
+
+        private void listview()
+        {
+            LV.Clear();
+            LV.View = View.Details;
+            LV.Columns.Add("Name of process", 80); 
+            LV.Columns.Add("Arriving time", 80); 
+            LV.Columns.Add("Duration of Execution", 80); 
+            LV.Columns.Add("Start", 80); 
+            LV.Columns.Add("Done", 80); 
+            LV.Columns.Add("Wait", 80); 
+            LV.Columns.Add("TA", 80);
+            LV.GridLines = true;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random(); int arrived, longt;
+            if (txtJlhproses.Text == "")
+                MessageBox.Show("Input Number of Processes !!!");
+            else
+            {
+                for (int i = 0; i < int.Parse(txtJlhproses.Text); i++)
+                {
+                    Array.Resize(ref data, data.Length + 1);
+                    arrived = rnd.Next(1, 30);
+                    longt = rnd.Next(1, 30);
+                    LV.Items.Add(data[data.GetUpperBound(0)].nama = "Proses - " + (i + 1));
+                    LV.Items[LV.Items.Count - 1].SubItems.Add(Convert.ToString((data[data.GetUpperBound(0)].arrived = arrived)));
+                    LV.Items[LV.Items.Count - 1].SubItems.Add(Convert.ToString((data[data.GetUpperBound(0)].longt = longt)));
+                }
+                txtJlhproses.ResetText();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listview();
+            int WT, LE;
+            string NP;
+            float total = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (data[i].longt < data[j].longt)
+                    {
+                        WT = data[j].arrived;
+                        data[j].arrived = data[i].arrived;
+                        data[i].arrived = WT;
+                        NP = data[j].nama;
+                        data[j].nama = data[i].nama;
+                        data[i].nama = NP;
+                        LE = data[j].longt;
+                        data[j].longt = data[i].longt;
+                        data[i].longt = LE;
+                    }
+                }
+            }
+            for (int r = 0; r < data.Length; r++)
+            {
+                if (r > 0)
+                {
+                    if (data[r].arrived <= data[r - 1].donet)
+                    {
+                        data[r].startt = data[r - 1].donet;
+                    }
+                    else
+                    {
+                        data[r].startt = data[r].arrived;
+                    }
+                }
+                else
+                    data[r].startt = data[r].arrived;
+
+                data[r].donet = data[r].longt + data[r].startt;
+                data[r].waitt = data[r].startt - data[r].arrived;
+                data[r].TA = data[r].donet - data[r].arrived;
+                total += data[r].TA;
+            }
+            txttotal.Text = total.ToString();
+            txtrata.Text = (total / data.Length).ToString();
+            for (int r = 0; r < data.Length; r++)
+            {
+                LV.Items.Add(data[r].nama);
+                LV.Items[r].SubItems.Add(data[r].arrived.ToString());
+                LV.Items[r].SubItems.Add(data[r].longt.ToString());
+                LV.Items[r].SubItems.Add(data[r].startt.ToString());
+                LV.Items[r].SubItems.Add(data[r].donet.ToString());
+                LV.Items[r].SubItems.Add(data[r].waitt.ToString());
+                LV.Items[r].SubItems.Add(data[r].TA.ToString());
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            data = new Data[0];
+            txtrata.Clear();
+            txttotal.Clear();
+            listview();
+        }
+
+    }
+}
